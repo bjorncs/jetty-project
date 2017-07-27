0ab0ce1c8af8a907648f224270716be7d9698561
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-07-27 13:36:00 +0200

    Use correct build script

--- travis.sh
+++ travis.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker pull vespaengine/vespa-dev:latest
-docker run --rm -v $(pwd):/source --entrypoint /source/build.sh vespaengine/vespa-dev:latest
+docker run --rm -v $(pwd):/source --entrypoint /source/travis-build.sh vespaengine/vespa-dev:latest

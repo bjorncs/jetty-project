bf4c05e6090737976e038afd50d8ae499c743f05
Bjørn Christian Seime <bjorncs@yahoo-inc.com>
2017-07-27 13:18:39 +0200

    Fix command

--- travis.sh
+++ travis.sh
@@ -2,4 +2,4 @@
 set -e
 
 docker pull vespaengine/vespa-dev:latest
-docker run --rm -v .:/source --entrypoint /source/build.sh vespaengine/vespa-dev:latest
+docker run --rm -v $(pwd):/source --entrypoint /source/build.sh vespaengine/vespa-dev:latest
